3b92f578ca39998a4d5bae66e6d9cfa8be4e3b47
sergejgasjuk <sergejgasjuk@gmail.com>
2015-11-04 22:52:49 +0200

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,67 +4,79 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     runSequence = require('run-sequence'),
     clean = require('gulp-clean'),
-    webserver = require('gulp-webserver'),
-    opn = require('opn');
+    connect = require('gulp-connect'),
+    open = require('gulp-open'),
+    gutil = require('gulp-util');
 
-var DEST = './dest/';
-var SRC = './src/';
-var server = {
-  host: 'localhost',
-  port: '8001'
-};
+var DEST_FOLDER = './dest/';
+var SRC_FOLDER = './src/';
+var PORT = 8001;
 
 gulp.task('clean', function(){
-  return gulp.src(DEST, {read: false})
+  return gulp.src(DEST_FOLDER, {read: false})
     .pipe(clean());
 });
 
 gulp.task('server', function(){
-  return gulp.src(DEST)
-    .pipe(webserver({
-      host: server.host,
-      port: server.port,
-      livereload: true,
-      directoryListing: false
-    }));
+  connect.server({
+    root: DEST_FOLDER,
+    port: PORT,
+    livereload: true
+  })
 });
 
 gulp.task('openURL', function(){
-  opn('http://' + server.host + ':' + server.port);
+  gulp.src(DEST_FOLDER)
+    .pipe(open({uri: 'http://localhost:' + PORT}))
 });
 
-gulp.task('compile-js', function() {
-  return gulp.src(SRC + 'js/**/*.js')
+gulp.task('process-vendors', function(){
+  return gulp.src([
+    "node_modules/react/dist/react.js",
+    "node_modules/react/dist/react-with-addons.js",
+    "node_modules/requirejs/require.js"
+  ])
+    .pipe(gulp.dest(DEST_FOLDER + "js/vendors"))
+});
+
+gulp.task('process-js', function() {
+  return gulp.src(SRC_FOLDER + 'js/**/*.js')
     .pipe(plumber())
     .pipe(babel({modules: "amd"}))
     .pipe(plumber.stop())
-    .pipe(gulp.dest(DEST + 'js/'));
+    .pipe(gulp.dest(DEST_FOLDER + 'js/'))
+    .pipe(connect.reload());
 });
 
-gulp.task('compile-html', function() {
-  return gulp.src(SRC + 'index.html')
-    .pipe(gulp.dest(DEST));
+gulp.task('process-html', function() {
+  return gulp.src(SRC_FOLDER + 'index.html')
+    .pipe(gulp.dest(DEST_FOLDER))
+    .pipe(connect.reload());
 });
 
-gulp.task('compile-sass', function() {
-  return gulp.src(SRC + 'style/*.scss')
+gulp.task('process-sass', function() {
+  return gulp.src(SRC_FOLDER + 'style/*.scss')
     .pipe(plumber())
     .pipe(sass())
-    .pipe(gulp.dest(DEST + 'style/'));
+    .pipe(plumber.stop())
+    .pipe(gulp.dest(DEST_FOLDER + 'style/'))
+    .pipe(connect.reload());
 });
 
 gulp.task('watch', function(){
-  gulp.watch(SRC + 'js/**/*.js', ['compile-js']);
-  gulp.watch(SRC + 'style/**/*.scss', ['compile-sass']);
-  gulp.watch(SRC + 'index.html', ['compile-html']);
+  gulp.watch(SRC_FOLDER + 'js/**/*.js', ['process-js']);
+  gulp.watch(SRC_FOLDER + 'style/**/*.scss', ['process-sass']);
+  gulp.watch(SRC_FOLDER + 'index.html', ['process-html']);
 });
 
 gulp.task('default', ['clean'], function(){
   runSequence(
-    ['compile-html', 'compile-js', 'compile-sass'],
-    //['server', 'watch'],
-    //'openURL'
-    'watch'
+    ['process-html', 'process-vendors', 'process-js', 'process-sass'],
+    ['server', 'watch'],
+    'openURL',
+    function(){
+      gutil.log("Server Started!")
+    }
   )
 });
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,24 @@
-var text = `first
-init
-!!!`;
+var genArray = function(m = 10) {
+  var arr = [];
+  var n = 0;
 
-setTimeout( () => {
-  alert(text);
+  while (n !== m) {
+    arr.push(n);
+    n+=1;
+  }
+  return arr;
+};
+
+var arr = genArray();
+
+var i = 0;
+var timer = window.setInterval(() => {
+  var div = document.createElement("div");
+  div.innerHTML = arr[i];
+  document.body.appendChild(div);
+  if (i === arr.length-1) {
+    window.clearInterval(timer);
+    return false;
+  }
+  i+=1;
 }, 1000);
