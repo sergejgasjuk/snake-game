0ca2589f59177c1969d3654fce1e7ba018dcd13b
Serhiy Hasiuk <Serhiy.Hasiuk@symphony-solutions.eu>
2015-11-05 16:33:36 +0200

    added food, snake

--- src/js/main.js
+++ src/js/main.js
@@ -17,16 +17,26 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.state.cols = COLS;
-    this.state.rows = ROWS;
+    // grid
+    this.state.cols = COLS; // grid width
+    this.state.rows = ROWS; // grid height
     this.state.grid = utls.generateGrid.apply(this, [this.state.cols, this.state.rows, EMPTY_CELL_VAL]);
+    // direction
+    this.state.direction = 'up';
+    // snake
+    this.state.snake = this.generateSnakeObject(this.state.direction);
+    this.state.snake.forEach((cell, i) => this.state.grid[cell.y][cell.x] = SNAKE_CELL_VAL);
+    // food
+    this.state.emptyCells = this.getEmptyCells(this.state.grid);
+    this.state.randomPos = utls.getRandomPos.apply(this, [this.state.emptyCells]);
+    this.state.grid[this.state.randomPos.y][this.state.randomPos.x] = FOOD_CELL_VAL;
+
   }
 
-  getEmptyCells() {
+  getEmptyCells(arr) {
     let emptyCells = [];
-    let grid = this.state.grid;
 
-    grid.forEach((row, y) =>
+    arr.forEach((row, y) =>
       row.filter((cell, x) => {
         if (cell === EMPTY_CELL_VAL) {
           emptyCells.push({x, y});
@@ -37,16 +47,101 @@ class Game extends React.Component {
     return emptyCells;
   }
 
+  setRandomDirection() {
+    let dir = '';
+    let randomInt = utls.getRandomInt(0, 4);
+
+    switch (randomInt) {
+      case 0:
+        dir = 'left';
+        break;
+      case 1:
+        dir = 'up';
+        break;
+      case 2:
+        dir = 'right';
+        break;
+      case 3:
+        dir = 'down';
+        break;
+      default:
+        dir = 'up';
+    }
+
+    return dir;
+  }
+
+  generateSnakeObject(direction) {
+    let snake = [],
+        length = 2, // snake length
+        x,
+        y;
+
+    switch (direction) {
+      case 'left':
+        x = utls.getRandomInt(length, this.state.cols);
+        y = utls.getRandomInt(length, this.state.rows);
+
+        for (let i = 0 ; i < length ; i += 1) {
+          snake.unshift({x: x, y: y - i});
+        }
+
+        break;
+      case 'up':
+        x = utls.getRandomInt(0, this.state.cols);
+        y = utls.getRandomInt(length, this.state.rows);
+
+        for (let i = 0 ; i < length ; i += 1) {
+          snake.unshift({x: x, y: y - i});
+        }
+
+        break;
+      case 'right':
+        x = utls.getRandomInt(length-1, this.state.cols - 1);
+        y = utls.getRandomInt(0, this.state.rows);
+
+        for (let i = 0 ; i < length ; i += 1) {
+          snake.push({x: x - i, y: y});
+        }
+
+        break;
+      case 'down':
+        x = utls.getRandomInt(0, this.state.cols);
+        y = utls.getRandomInt(length-1, this.state.rows - 1);
+
+        for (let i = 0 ; i < length ; i += 1) {
+          snake.push({x: x, y: y - i});
+        }
+
+        break;
+    }
+
+    return snake;
+
+  }
+
   render() {
-    let e = this.getEmptyCells();
+    let e = this.state.snake;
     console.log(e);
+
+    // TODO: refactor
+    let setCellClassName = (val) => {
+      if (val === SNAKE_CELL_VAL) {
+        return "field-cell--snake";
+      } else if (val === FOOD_CELL_VAL) {
+        return "field-cell--food";
+      } else {
+        return '';
+      }
+    };
+
     return (
       <div className="game">
         <div className="game__field">
           {this.state.grid.map((row, y) =>
             <div className="field-row" key={`row_${y}`}>
               {row.map((cell, x) =>
-                <div className="field-cell" key={`cell_y=${y}_x=${x}`}></div>
+                <div className={`field-cell ` + setCellClassName(cell)} key={`cell_y=${y}_x=${x}`}></div>
               )}
             </div>
           )}
